a00302519202578a4efcee7e722e9f258450c952
Ale Santoro <alexi.santoro@gmail.com>
2024-08-26 01:05:17 +0200

    added audios for modules 3,4

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     let lines = {};
 
@@ -20,12 +19,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 const card = document.createElement('div');
                 const text = document.createElement('h1');
                 
-                
                 const randIndex = Math.floor(Math.random() * lines.length);
                 const selectedLine = JSON.stringify(
-                    lines[randIndex])
-                    .replace(/[{}",]/g, '').split(":");
-
+                    lines[randIndex]).replace(/[{}",]/g, '').split(":");
                 strEn = selectedLine[0];
                 strSw = selectedLine[1];
 
@@ -37,7 +33,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 next.textContent = 'Next';
                 next.id = 'next';   
 
-                // Add event listener to display a new random line on button click
                 next.addEventListener('click', displayRandomLine);
 
                 const check = document.createElement('button');
@@ -49,20 +44,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 check.addEventListener('click', ()=>{
                     text.textContent = strSw
-                    play.style.visibility = 'visible';
-                })
+                    play.style.visibility = 'visible';})
 
                 play.addEventListener('click', ()=>{
-                    (new Audio("cards/audio/" + module + "/" + randIndex + ".mp3")).play();
-                });
+                    (new Audio("cards/audio/" + module + "/" + randIndex + ".mp3")).play();});
 
                 fileContentDiv.appendChild(card);
                 card.appendChild(document.createElement('br'));
                 card.appendChild(check);
                 card.appendChild(play);
                 card.appendChild(next);
-                card.appendChild(document.createElement('br'));
-                card.appendChild(document.createElement('br'));
             } else {
                 const noMoreLines = document.createElement('p');
                 noMoreLines.textContent = "No more cards available.";
@@ -72,21 +63,8 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Add event listeners to buttons
-    document.getElementById('menu-1').addEventListener('click', () => {
-        fetchAndDisplayKeys('1');
-    });
-
-    document.getElementById('menu-2').addEventListener('click', () => {
-        fetchAndDisplayKeys('2');
-    });
-    
-    document.getElementById('menu-3').addEventListener('click', () => {
-        fetchAndDisplayKeys('3');
-    });
-    
-    document.getElementById('menu-4').addEventListener('click', () => {
-        fetchAndDisplayKeys('4');
-    });
-    
-    
+    document.getElementById('menu-1').addEventListener('click', () => {fetchAndDisplayKeys('1');});
+    document.getElementById('menu-2').addEventListener('click', () => {fetchAndDisplayKeys('2');});
+    document.getElementById('menu-3').addEventListener('click', () => {fetchAndDisplayKeys('3');});
+    document.getElementById('menu-4').addEventListener('click', () => {fetchAndDisplayKeys('4');});
 });
